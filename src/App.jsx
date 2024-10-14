df998925391699c5525538d3f62a41d1d6cadb47
Zpillsbury <zpillbury98@gmail.com>
2024-10-14 15:20:59 -0400

    imports for confirming deletion

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import Divider from "@mui/material/Divider"
 import IconButton from "@mui/material/IconButton"
 import DeleteIcon from "@mui/icons-material/Delete"
 import { v4 as uuidv4 } from "uuid"
+import Dialog from "@mui/material/Dialog"
+import DialogActions from "@mui/material/DialogActions"
+import DialogContent from "@mui/material/DialogContent"
+import DialogContentText from "@mui/material/DialogContentText"
+import DialogTitle from "@mui/material/DialogTitle"
 
 import "./App.css"
 
